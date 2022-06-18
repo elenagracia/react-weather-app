166d71c832565d410fbde0e1dda0430792313539
Elena Gracia <91082073+elenagracia@users.noreply.github.com>
2022-06-18 17:26:40 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 
 export default function App() {
   return <div className='App'>
-    Weather App
+    <div className='container'>
+   <h1> Weather App</h1>
   <footer>
     This project was coded by Elena Gracia and <a href="https://github.com/elenagracia/react-weather-app" target="_blank">open-sourced on GitHub</a>
   </footer>
+  </div>
   </div>
 }

35e476ef9c2a45467e426dc44a30af70e3f2f7e4
Elena Gracia <91082073+elenagracia@users.noreply.github.com>
2022-06-18 17:07:20 +0100

    Added the Github Repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,10 @@ import React from 'react';
 import './App.css';
 
 export default function App() {
-  return <div className='App'>Weather App</div>
+  return <div className='App'>
+    Weather App
+  <footer>
+    This project was coded by Elena Gracia and <a href="https://github.com/elenagracia/react-weather-app" target="_blank">open-sourced on GitHub</a>
+  </footer>
+  </div>
 }
